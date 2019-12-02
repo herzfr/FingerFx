a3527b2f555001a8cfa258568a7481927a48cf1b
herzfr <47446320+herzfr@users.noreply.github.com>
2019-12-02 09:49:59 +0700

    herza

---
+++ src/main/java/com/travest/fingerfx/Service/LoginService.java
@@ -0,0 +1,24 @@
+package com.travest.fingerfx.Service;
+
+import okhttp3.*;
+
+import java.io.IOException;
+
+public class LoginService {
+    
+
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    OkHttpClient client = new OkHttpClient();
+
+    String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(json, JSON);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+}
