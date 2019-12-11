e813c8618e150966b9e10cbe1c6618ae5e91e2bc
Dwi Nepatyaz <38588473+nepatyaz@users.noreply.github.com>
2019-12-11 14:47:54 +0700

    update

--- src/main/java/com/travest/fingerfx/FingerMainApp.java
+++ src/main/java/com/travest/fingerfx/FingerMainApp.java
@@ -5,6 +5,7 @@ import SecuGen.FDxSDKPro.jni.JSGFPLib;
 import SecuGen.FDxSDKPro.jni.SGDeviceInfoParam;
 import com.travest.fingerfx.utility.AppData;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,6 +18,10 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import sun.misc.BASE64Decoder;
 
+
+import javax.swing.*;
+import java.awt.font.ImageGraphicAttribute;
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.URL;
@@ -33,8 +38,24 @@ public class FingerMainApp implements Initializable {
     private long deviceName;
     private long devicePort;
 
-    AppData appData ;
+    byte[] imageBuffer1;
+    byte[] imageBuffer2;
 
+    private BufferedImage imgReg1;
+    private BufferedImage imgReg2;
+    private BufferedImage imgVerify;
+
+    private byte[] regMin1 = new byte[400];
+    private byte[] regMin2 = new byte[400];
+    private byte[] vrfMin = new byte[400];
+
+    private boolean r1Captured = false;
+    private boolean r2Captured = false;
+    private boolean v1Captured = false;
+
+
+
+    AppData appData;
 
 
     @FXML
@@ -94,9 +115,16 @@ public class FingerMainApp implements Initializable {
     @FXML
     private Button btnDisc;
     @FXML
+    private Button testCapture;
+    @FXML
     private Label lblAccount;
     @FXML
     private ImageView imgView;
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private ImageView imageFinger1;
+
 
     @FXML
     void onConnection(ActionEvent event) {
@@ -132,7 +160,7 @@ public class FingerMainApp implements Initializable {
         regLog.setStyle("-fx-fill: #fff;");
         clsLog.setStyle("-fx-fill: #2bc344;");
 
-        if (jsgfpLib != null ) {
+        if (jsgfpLib != null) {
             jsgfpLib.CloseDevice();
             jsgfpLib.Close();
             jsgfpLib = null;
@@ -142,7 +170,7 @@ public class FingerMainApp implements Initializable {
             lblInfo.setText("OFF");
             incubLight.setStyle(" -fx-fill: #e12272;");
         } else {
-            displayCon.setText( "CloseDevice() Error : " + ret);
+            displayCon.setText("CloseDevice() Error : " + ret);
         }
 
     }
@@ -193,11 +221,10 @@ public class FingerMainApp implements Initializable {
         }
 
 
-
     }
 
 
-    private void initialize(){
+    private void initialize() {
         displayCon.setText("");
         displayCon.appendText("Initialize Device... \n");
 
@@ -220,7 +247,7 @@ public class FingerMainApp implements Initializable {
         ret = jsgfpLib.Init(deviceName);
 
         System.out.println(ret);
-        if (ret == 6 ) {
+        if (ret == 6) {
             displayCon.appendText("Device not detected \n");
         } else {
             displayCon.appendText("Device detected \n");
@@ -229,7 +256,7 @@ public class FingerMainApp implements Initializable {
         ret = jsgfpLib.EnumerateDevice(ndevs, devList);
         displayCon.appendText("Enumerate Device Called\n");
 
-        for (int i = 0 ; i < ndevs[0]; i++) {
+        for (int i = 0; i < ndevs[0]; i++) {
             ret = jsgfpLib.Init(devList[i].devName);
             displayCon.appendText("Device Init : " + ret + "\n");
 
@@ -263,7 +290,6 @@ public class FingerMainApp implements Initializable {
                     devicePort = dvcInfo.comPort;
                     lblInfo.setText("ON");
                     incubLight.setStyle(" -fx-fill: #2bc344;");
-
 
 
                 } else {
@@ -304,5 +330,85 @@ public class FingerMainApp implements Initializable {
 
     }
 
+
+    @FXML
+    void testFingerCapture(ActionEvent event) {
+
+
+        System.out.println("halo test capture");
+
+        int[] quality = new int[1];
+        int[] numOfMinutiae = new int[1];
+        long nfiqvalue;
+        imgReg2 = new BufferedImage(dvcInfo.imageWidth, dvcInfo.imageHeight, BufferedImage.TYPE_BYTE_GRAY);
+        imageBuffer2 = ((java.awt.image.DataBufferByte) imgReg2.getRaster().getDataBuffer()).getData();
+
+        Image image = SwingFXUtils.toFXImage(imgReg2, null);
+
+        if (jsgfpLib != null) {
+            ret = jsgfpLib.GetImageEx(imageBuffer2, 10 * 1000, 0, 80);
+            System.out.println(new String(imageBuffer2));
+
+            jsgfpLib.GetImageQuality(dvcInfo.imageHeight, dvcInfo.imageWidth, imageBuffer2, quality);
+            SGFingerInfo fingerInfo = new SGFingerInfo();
+            fingerInfo.FingerNumber = SGFingerPosition.SG_FINGPOS_LI;
+            fingerInfo.ImageQuality = quality[0];
+            fingerInfo.ImpressionType = SGImpressionType.SG_IMPTYPE_LP;
+            fingerInfo.ViewNumber = 1;
+
+            if (ret == SGFDxErrorCode.SGFDX_ERROR_NONE) {
+                textArea.appendText("Tahap 2 Second Capture\n");
+                System.out.println("tahap 2 second capture ");
+                imageFinger1.setImage(image);
+//                JlabelImage2.setIcon(new ImageIcon(imgReg2.getScaledInstance(130, 150, java.awt.Image.SCALE_DEFAULT)));
+
+                if (quality[0] < 45) {
+                    textArea.appendText("GetImageEx() Success [" + ret + "] but image quality is [" + quality[0] + "]. Please try again\n");
+                    System.out.println("GetImageEx() Success [" + ret + "] but image quality is [" + quality[0] + "]. Please try again\n");
+                } else {
+                    textArea.appendText("GetImageEx() Success [" + ret + "] but image quality is [" + quality[0] + "]. Thank u\n");
+                    System.out.println("GetImageEx() Success [" + ret + "] but image quality is [" + quality[0] + "]. Thank u\n");
+
+                    ret = jsgfpLib.CreateTemplate(fingerInfo, imageBuffer2, regMin2);
+                    System.out.println("create template " + new String(regMin2) );
+                    textArea.appendText("\ncreate template " + new String(regMin2) +"\n");
+
+                    if (ret == SGFDxErrorCode.SGFDX_ERROR_NONE) {
+                        long ret2 = jsgfpLib.GetImageQuality(dvcInfo.imageHeight, dvcInfo.imageWidth, imageBuffer2, quality);
+                        nfiqvalue = jsgfpLib.ComputeNFIQ(imageBuffer2, dvcInfo.imageWidth, dvcInfo.imageHeight);
+                        ret2 = jsgfpLib.GetNumOfMinutiae(SGFDxTemplateFormat.TEMPLATE_FORMAT_SG400, regMin2, numOfMinutiae);
+
+                        System.out.println(quality[0]);
+                        System.out.println(nfiqvalue);
+                        System.out.println(numOfMinutiae);
+
+                        if ((quality[0] >= 45) && (nfiqvalue <= 2) && (numOfMinutiae[0] >= 20)) {
+                            textArea.appendText("Verification Capture PASS QC. Quality[" + quality[0] + "] NFIQ[" + nfiqvalue + "] Minutiae[" + numOfMinutiae[0] + "]\n");
+                            System.out.println("Verification Capture PASS QC. Quality[" + quality[0] + "] NFIQ[" + nfiqvalue + "] Minutiae[" + numOfMinutiae[0] + "]\n");
+                            r2Captured = true;
+                            System.out.println("ret value : "+ ret);
+                        } else {
+                            textArea.appendText("Verification Capture FAIL QC. Quality[" + quality[0] + "] NFIQ[" + nfiqvalue + "] Minutiae[" + numOfMinutiae[0] + "]\n");
+                            System.out.println("Verification Capture FAIL QC. Quality[" + quality[0] + "] NFIQ[" + nfiqvalue + "] Minutiae[" + numOfMinutiae[0] + "]\n");
+                        }
+
+                    } else {
+                        System.out.println("CreateTemplate() Error : " + ret + "\n");
+                        textArea.appendText("CreateTemplate() Error : " + ret + "\n");
+                    }
+                }
+
+            } else {
+                textArea.appendText("Failed #1");
+                System.out.println("failed");
+            }
+
+        } else {
+            textArea.appendText("Errorr Guysss jsqfpLib nya \n");
+            System.out.println("Error jsgfplibnya ");
+        }
+
+
+    }
 
 }
